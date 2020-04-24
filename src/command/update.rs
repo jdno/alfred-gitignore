6019240d2885cc6f553812578aeb547658ba2ec0
Jan David <jandavid@6a64.com>
2020-04-24 19:05:01 +0200

    Reimplement update command
    The update command has been reimplemented with a bit more structure. It
    can return its own item, and prints a success message once the update is
    done.

---
+++ src/command/update.rs
@@ -0,0 +1,34 @@
+use crate::exit_with_error;
+use crate::repository::Repository;
+use alfred::{Item, ItemBuilder};
+use std::io::stdout;
+use std::process::exit;
+
+pub struct Update<'a> {
+    _lifetime: &'a str,
+}
+
+impl<'a> Update<'a> {
+    pub fn item() -> Item<'a> {
+        ItemBuilder::new("Update gitignore templates")
+            .subtitle("Download the latest templates from github/gitignore")
+            .into_item()
+    }
+
+    pub fn perform(repository: &Repository) -> ! {
+        match repository.update() {
+            Ok(_) => {
+                alfred::json::write_items(
+                    stdout(),
+                    &[ItemBuilder::new("Successfully updated the templates")
+                        .subtitle("The latest templates from github/gitignore have been downloaded")
+                        .into_item()],
+                )
+                .unwrap();
+
+                exit(0);
+            }
+            Err(error) => exit_with_error(&error),
+        }
+    }
+}
